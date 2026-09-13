42f81df3527a1c276df66fefa4645d2631fcb510
rxmi-bkd <boukadidarami0@gmail.com>
2026-09-13 20:43:10 +0200

    save

--- src/main/java/org/bkd/saas/password_reset/services/PasswordResetService.java
+++ src/main/java/org/bkd/saas/password_reset/services/PasswordResetService.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.bkd.saas.user.responses.UserWithPasswordResponse;
 import org.bkd.saas.user.services.UserPasswordService;
 import org.bkd.saas.user.services.UserService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,18 +23,19 @@ public class PasswordResetService {
   private final PasswordResetJwtService passwordResetJwtService;
 
   public void forgotPassword(String email) {
-    UserWithPasswordResponse user = userService.readUserWithPassword(email);
-    String jwt = passwordResetJwtService.createJwt(user.id(), user.password());
-    log.info("Password reset requested for {}: jwt={}", email, jwt);
+    try {
+      UserWithPasswordResponse user = userService.readUserWithPassword(email);
+      String jwt = passwordResetJwtService.createJwt(user.id(), user.password());
+      log.info("Password reset requested for {} : jwt={}", email, jwt);
+    } catch (UsernameNotFoundException e) {
+      log.info("User not found for {}", email);
+    }
   }
 
   public void resetPassword(String jwt, String newPassword) {
     Claims claims = passwordResetJwtService.readJwt(jwt);
     UUID userId = UUID.fromString(claims.getSubject());
     UserWithPasswordResponse user = userService.readUserWithPassword(userId);
-
-    if (passwordResetJwtService.isValidJwt(jwt, user.password())) {
-      userPasswordService.updateUserPassword(userId, newPassword);
-    }
+    if (passwordResetJwtService.isValidJwt(jwt, user.password())) userPasswordService.updateUserPassword(userId, newPassword);
   }
 }
